39706d4ef27c7a26d0c44e42e90f8c7fa98d559c
Bill Greer <bgreer5050@gmail.com>
2015-04-10 14:12:51 -0500

    Update StringHelper.cs

--- StringHelper.cs
+++ StringHelper.cs
@@ -1,12 +1,16 @@
- public static  class StringHelper
+public static  class StringHelper
     {
-
+       /// <summary>
+       /// Removes / \ { } " from string
+       /// </summary>
+       /// <param name="s"></param>
+       /// <returns></returns>
        public static string RemoveExtras(string s)
        {
            StringBuilder sb = new StringBuilder(500);
            foreach(char c in s.ToCharArray())
            {
-            if(c ==123 || c == 34 || c == 47 || c== 92)
+            if(c == 123  || c == 125 || c == 34 || c == 47 || c == 92)
             {
                 
 
